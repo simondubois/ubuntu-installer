c0c335db7eaea4899807e71368df5a99c67ea199
Simon Dubois <simon@dubandubois.com>
2015-10-08 19:39:25 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Install ansible
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install software-properties-common ansible git -y
 
 # Clone repository
-ansible -i "localhost," -c local -m apt -a "name=git state=latest"
-ansible -i "localhost," -c local -m git -a "repo=git://git@github.com:simondubois/ubuntu-installer.git dest=$HOME/ubuntu-installer version=HEAD"
+mkdir -p ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+git clone git://github.com/simondubois/ubuntu-installer.git
+
+# Deploy installation
+cd ubuntu-installer
