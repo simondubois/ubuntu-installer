d7d49898efd4e346d4a7cce1e0843921d4d8f519
Simon Dubois <simon@dubandubois.com>
2015-10-15 23:02:04 +0200

    Run apt as root

--- install/install.sh
+++ install/install.sh
@@ -1,19 +1,19 @@
  #!/bin/bash
 
 # Install ansible
-apt-add-repository ppa:ansible/ansible -y
-apt-get update
-apt-get install software-properties-common ansible git -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install software-properties-common ansible git -y
 
 # Allow sudo without password
-#echo "%sudo ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/ansible
-#chown root:root /etc/sudoers.d/ansible
-#chmod 0440 /etc/sudoers.d/ansible
+echo "%sudo ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ansible
+sudo chown root:root /etc/sudoers.d/ansible
+sudo chmod 0440 /etc/sudoers.d/ansible
 
 # Import github SSH key
 mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 # Deploy playbooks
-echo "localhost ansible_connection=local" | tee -a /etc/ansible/hosts
+echo "localhost ansible_connection=local" | sudo tee -a /etc/ansible/hosts
 ansible-pull -o -U git://github.com/simondubois/ubuntu-installer.git
