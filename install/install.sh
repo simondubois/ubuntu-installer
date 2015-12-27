9718d9f43d4be861c63442cf3eca35384ab95c98
Simon Dubois <simon@dubandubois.com>
2015-12-27 17:50:51 +0100

    Add PPA

--- install/install.sh
+++ install/install.sh
@@ -3,7 +3,7 @@
 # Install ansible
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt-get install software-properties-common ansible git -y
+sudo apt-get install software-properties-common ansible git python-apt -y
 
 # Allow sudo without password
 echo "%sudo ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/ansible
