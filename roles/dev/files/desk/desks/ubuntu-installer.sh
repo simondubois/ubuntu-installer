2f57f4a5d558de177bd6c92ff82e88be264c5110
Simon Dubois <simon@dubandubois.com>
2016-03-13 22:05:55 +0100

    Add desk task

--- roles/dev/files/desk/desks/ubuntu-installer.sh
+++ roles/dev/files/desk/desks/ubuntu-installer.sh
@@ -9,3 +9,8 @@ cd ~/www/ubuntu-installer
 install() {
   ln -s ~/.ansible/pull/$HOSTNAME ~/www/ubuntu-installer
 }
+
+# Run ansible pull locally
+run() {
+	ansible-pull -U git://github.com/simondubois/ubuntu-installer.git
+}
