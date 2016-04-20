d1204faba704b845fba49724e2885e4be60d29fe
Simon Dubois <simon@dubandubois.com>
2016-04-20 23:53:06 +0200

    Fix desk file

--- roles/dev/files/desk/desks/budget.sh
+++ roles/dev/files/desk/desks/budget.sh
@@ -9,3 +9,5 @@ PRJ_PATH="$HOME/www/$PRJ_NAME"
 source ~/.desk/desks/default.sh $PRJ_NAME $PRJ_PATH
 source ~/.desk/desks/web.sh $PRJ_NAME $PRJ_PATH
 source ~/.desk/desks/dubandubois.sh $PRJ_NAME $PRJ_PATH
+
+PATH=$(npm bin):$PATH
