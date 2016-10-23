a2a0824a65c962d6bdf263837081a9d55f54f4b9
Simon Dubois <simon@dubandubois.com>
2016-10-23 16:28:42 +0200

    Fix desk file localization

--- roles/desk/files/desks/budget.sh
+++ roles/desk/files/desks/budget.sh
@@ -6,6 +6,28 @@
 PRJ_NAME=budget
 PRJ_PATH="$HOME/www/$PRJ_NAME"
 
+if [ ! -f /.dockerenv ]; then
+    mkdir -p $PRJ_PATH
+
+    STATUS=$(docker inspect --format="{{ .State.Running }}" $PRJ_NAME 2> /dev/null)
+
+    if [ $? -eq 1 ]; then
+        docker run -t -d \
+          -p 80:80 \
+          -v $PRJ_PATH:/var/www/html \
+          --name $PRJ_NAME \
+          simondubois/$$PRJ_NAME
+    fi
+
+    if [ "$STATUS" == "false" ]; then
+        docker start $PRJ_NAME
+    fi
+
+    docker exec -ti -u $(id -u):$(id -g) $PRJ_NAME /bin/bash
+
+    exit
+fi
+
 source ~/.desk/desks/default.sh $PRJ_NAME $PRJ_PATH
 source ~/.desk/desks/web.sh $PRJ_NAME $PRJ_PATH
 source ~/.desk/desks/dubandubois.sh $PRJ_NAME $PRJ_PATH
